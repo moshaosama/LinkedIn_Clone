d46835aeeae562487960c846587f92afa3540dc9
moshaosama <mohamedOSFekry@gmail.com>
2025-01-18 22:56:17 +0200

    Create Posts

--- src/Components/add-post/add-post.component.ts
+++ src/Components/add-post/add-post.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PostComponent } from "../post/post.component";
 
 @Component({
   selector: 'app-add-post',
   standalone: true,
-  imports: [],
+  imports: [PostComponent],
   templateUrl: './add-post.component.html',
   styleUrl: './add-post.component.css'
 })
