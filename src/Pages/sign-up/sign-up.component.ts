6537ddaf0553f80f035b91ed813bdbf85318263c
moshaosama <mohamedOSFekry@gmail.com>
2025-01-18 19:18:30 +0200

    first commit

---
+++ src/Pages/sign-up/sign-up.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SignUpComponent {
+  constructor() {
+    console.log('Mosha2');
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { SignUpComponent } from '../Pages/sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [SignUpComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'linkedIn_Clone';
