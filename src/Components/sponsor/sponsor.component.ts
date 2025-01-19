0547b129aca456bacb2ebd60fcd86f88512b1548
moshaosama <mohamedOSFekry@gmail.com>
2025-01-19 18:20:41 +0200

    Create Links

--- src/Components/sponsor/sponsor.component.ts
+++ src/Components/sponsor/sponsor.component.ts
@@ -5,8 +5,36 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './sponsor.component.html',
-  styleUrl: './sponsor.component.css'
+  styleUrl: './sponsor.component.css',
 })
 export class SponsorComponent {
-
+  links = [
+    {
+      name: 'About',
+    },
+    {
+      name: 'Accessibility',
+    },
+    {
+      name: 'Help Center',
+    },
+    {
+      name: 'Privacy & Terms ',
+    },
+    {
+      name: 'Ad Choices',
+    },
+    {
+      name: 'Advertising',
+    },
+    {
+      name: 'Business Services ',
+    },
+    {
+      name: 'Get the LinkedIn app',
+    },
+    {
+      name: 'More',
+    },
+  ];
 }
