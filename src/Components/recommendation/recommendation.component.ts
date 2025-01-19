15d8fb838d4a831b91abe4af1a2128daef932981
moshaosama <mohamedOSFekry@gmail.com>
2025-01-19 17:58:30 +0200

    Create Games Section and Sponsor Section

--- src/Components/recommendation/recommendation.component.ts
+++ src/Components/recommendation/recommendation.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PersonComponent } from './person/person.component';
-import { GameCardComponent } from "../gameCard/gameCard.component";
+import { GameCardComponent } from '../gameCard/gameCard.component';
+import { SponsorComponent } from '../sponsor/sponsor.component';
 
 @Component({
   selector: 'app-recommendation',
   standalone: true,
-  imports: [PersonComponent, GameCardComponent],
+  imports: [PersonComponent, GameCardComponent, SponsorComponent],
   templateUrl: './recommendation.component.html',
   styleUrl: './recommendation.component.css',
 })
