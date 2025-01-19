1b61674fe6bd5ae7c32ebc3a37c0800b636ad3c4
moshaosama <mohamedOSFekry@gmail.com>
2025-01-20 00:27:49 +0200

    Edit

--- Server/Linkedin_Clone/src/main/java/com/example/Linkedin_Clone/Post/postRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.Linkedin_Clone.Post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface postRepository extends JpaRepository<Post , Integer> {
-}
