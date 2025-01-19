2fc29d89e8aa79939a21bfc6c2f873a3db8d8a2b
moshaosama <mohamedOSFekry@gmail.com>
2025-01-19 13:45:12 +0200

    Create Recommendation Card

--- src/Pages/home/home.component.ts
+++ src/Pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProfileCardComponent } from '../../Components/profile-card/profile-card.component';
 import { AddPostComponent } from '../../Components/add-post/add-post.component';
+import { RecommendationComponent } from '../../Components/recommendation/recommendation.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProfileCardComponent, AddPostComponent],
+  imports: [ProfileCardComponent, AddPostComponent, RecommendationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
