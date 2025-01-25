ce102a430dcc057db86dd6b58a9294ccc24f88d3
moshaosama <mohamedOSFekry@gmail.com>
2025-01-25 11:17:54 +0200

    Create Experience Section and Add Experience

--- src/Services/experience.service.ts
+++ src/Services/experience.service.ts
@@ -4,7 +4,7 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root',
 })
 export class ExperienceService {
-  ActiveExperience = signal(true);
+  ActiveExperience = signal(false);
   ActiveCreatePosition = signal(false);
 
   changeActiveExperience() {
